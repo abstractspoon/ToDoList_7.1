3023780d1594dadcc792791fb1efcd7e6148fb82
abstractspoon <daniel.godson@gmail.com>
2017-02-03 20:09:14 +1100

    Update app version

--- ToDoList/ToDoListWnd.cpp
+++ ToDoList/ToDoListWnd.cpp
@@ -4583,7 +4583,7 @@ CString CToDoListWnd::GetTitle(BOOL bExtended)
 
 CString CToDoListWnd::GetVersion(BOOL bExtended)
 {
-	static CString sVersion("7.1.0.11");
+	static CString sVersion(FileMisc::GetAppVersion());
 	CLocalizer::IgnoreString(sVersion);
 
 	if (!bExtended)
